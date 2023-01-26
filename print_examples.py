da3292a3461f6ad507cc20b37478aeec566dbabf
mariamerino <mmerino.ieu2021@student.ie.edu>
2023-01-26 10:59:49 +0100

    This is the thrid class example (printing)

---
+++ print_examples.py
@@ -0,0 +1,7 @@
+print("This is a text")
+print("this is another",end="I want this at the end") #instead of adding an ending the phrase separates both code lines
+print("this is another",end=" |I want this at the end|\n") #n makes the next line go down
+print("This", "can", "be","written","like","this")
+print(2+2) #mathematical equations
+
+print("This", "can", "be","written","like","this",sep="!") #sep tells you how words are separated
